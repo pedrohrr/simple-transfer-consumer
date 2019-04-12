ba66ae0f3b6543fcdabba7b42ccfc5c2f25a15c5
Pedro Rodrigues <pedrorodrigues@Pedros-MacBook-Pro.local>
2019-04-12 10:52:19 +0200

    Initial setup

---
+++ src/main/kotlin/com/pedrohrr/simpletransfer/simpletransferconsumer/SimpleTransferConsumerApplication.kt
@@ -0,0 +1,11 @@
+package com.pedrohrr.simpletransfer.simpletransferconsumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleTransferConsumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleTransferConsumerApplication>(*args)
+}
---
+++ src/test/kotlin/com/pedrohrr/simpletransfer/simpletransferconsumer/SimpleTransferConsumerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.pedrohrr.simpletransfer.simpletransferconsumer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SimpleTransferConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
